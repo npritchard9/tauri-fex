a93c1a5a1c0aebd4b3db5aea22445cd16f52362d
Noah Pritchard <noahpritchard9@gmail.com>
2023-06-22 17:24:06 -0400

    add images and search

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,16 @@ import {
 import { invoke } from "@tauri-apps/api/tauri";
 import { Fex } from "../src-tauri/bindings/Fex";
 import { FexFile } from "../src-tauri/bindings/FexFile";
+import FolderIcon from "./assets/folder";
+import DocumentIcon from "./assets/document";
 
 const [file, setFile] = createSignal<string>("");
 const [dir, setDir] = createSignal<String[]>([""]);
 const [filePath, setFilePath] = createSignal<String[]>([]);
 const [showHidden, setShowHidden] = createSignal(false);
+const [search, setSearch] = createSignal<string>("");
+
+const cd = () => dir().at(-1);
 
 async function read_file() {
   let f: string = await invoke("send_file", { path: filePath().join("/") });
@@ -23,6 +28,11 @@ async function read_file() {
 
 function App() {
   const [files, setFiles] = createSignal<FexFile[]>([]);
+
+  const filtered_files = () =>
+    files().filter((f) =>
+      f.name.toLowerCase().includes(search().toLowerCase())
+    );
 
   async function get_files() {
     let fex: Fex = await invoke("send_dir", { dir: dir().join("/") });
@@ -47,12 +57,20 @@ function App() {
           onclick={() => {
             setFilePath([]);
             setFile("");
-            setDir((d) => d.filter((p) => p !== dir().at(-1)));
+            setSearch("");
+            setDir((d) => d.filter((p) => p !== cd()));
           }}
           disabled={dir().length === 1}
         >
           &larr;
         </button>
+        <input
+          type="text"
+          placeholder={`Search ${cd() === "" ? "Home" : cd()}`}
+          value={search()}
+          oninput={(e) => setSearch(e.currentTarget.value)}
+          class="bg-gray-600 text-white placeholder-white rounded-xl p-2"
+        />
         <div>{dir().join("/")}</div>
         <div class="text-4xl">Fex</div>
         <div class="flex items-center justify-center gap-2">
@@ -72,13 +90,19 @@ function App() {
           </pre>
         }
       >
-        <For each={files()}>{(f) => <File {...f} />}</For>
+        <Show
+          when={search()}
+          fallback={<For each={files()}>{(f) => <File {...f} />}</For>}
+        >
+          <For each={filtered_files()}>{(f) => <File {...f} />}</For>
+        </Show>
       </Show>
     </div>
   );
 }
 
 function handle_click_entry(f: FexFile) {
+  setSearch("");
   if (f.file_type === "Dir" || f.file_type === "HiddenDir") {
     setDir((d) => [...d, f.name]);
   } else {
@@ -88,6 +112,12 @@ function handle_click_entry(f: FexFile) {
 }
 
 const File = (f: FexFile) => {
+  let icon =
+    f.file_type === "File" || f.file_type === "HiddenFile" ? (
+      <DocumentIcon />
+    ) : (
+      <FolderIcon />
+    );
   return (
     <Switch>
       <Match when={showHidden()}>
@@ -95,6 +125,7 @@ const File = (f: FexFile) => {
           class="flex items-center justify-start gap-4 mx-2 p-2 hover:bg-gray-800 hover:rounded-xl"
           onclick={() => handle_click_entry(f)}
         >
+          <span>{icon}</span>
           <span class="font-bold overflow-x-scroll w-1/3">{f.name}</span>
           <span class="text-amber-300 w-16">{f.len as unknown as number}</span>
           <span class="text-blue-300">
@@ -108,6 +139,7 @@ const File = (f: FexFile) => {
             class="flex items-center justify-start gap-4 mx-2 p-2 hover:bg-gray-800 hover:rounded-xl"
             onclick={() => handle_click_entry(f)}
           >
+            <span>{icon}</span>
             <span class="font-bold overflow-x-scroll w-1/3">{f.name}</span>
             <span class="text-amber-300 w-16">
               {f.len as unknown as number}
